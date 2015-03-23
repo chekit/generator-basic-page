2bb641edbeabcf5bd40ee92a0d14c8cfc99910ff
Anton Cherepov <chekitdnb@gmail.com>
2015-03-23 10:15:48 +0300

    Update watch.gulp.js

--- app/templates/gulp/watch.gulp.js
+++ app/templates/gulp/watch.gulp.js
@@ -8,7 +8,7 @@ gulp.task('watch', function () {
 	gulp.watch('./src/sass/base/fonts/**/*', ['fonts', browserSync.reload]);
 	gulp.watch('./src/*.html', ['html', browserSync.reload]);
 	gulp.watch('bower.json', ['wiredep', browserSync.reload]);
-	gulp.watch('./src/jade/**/*.jade', ['jade', browserSync.reload]);
+	gulp.watch('./src/jade/**/*', ['jade', browserSync.reload]);
 	gulp.watch('./src/js/*.js', ['js', browserSync.reload]);
 	gulp.watch('./src/images/**/*.{png,jpg,gif,svg,json,xml,ico}', ['images', browserSync.reload]);
 });
