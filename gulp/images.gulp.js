b1c4259e1ba8088a5b6a4fc4d0813eb519f49b46
Anton Cherepov <chekitdnb@gmail.com>
2017-07-05 00:35:32 +0300

    add improvements

--- gulp/images.gulp.js
+++ gulp/images.gulp.js
@@ -4,5 +4,10 @@ import gulp  from 'gulp';
 
 gulp.task('images', () => {
 	return gulp.src(`${config.paths.img_src}`)
+		.pipe(cache(imagemin({ 
+			optimizationLevel: 3, 
+			progressive: true, 
+			interlaced: true 
+		})))
 		.pipe(gulp.dest(`${config.paths.img_dist}`));
 })
